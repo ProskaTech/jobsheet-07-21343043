a31be98cdb41d3f3f15ea645f45e7a9c998bab24
21343043 <113020517+21343043@users.noreply.github.com>
2022-10-20 20:09:51 +0700

    Add files via upload

---
+++ src/tujuhtujuh/Tugas/AngkaDo_While.java
@@ -0,0 +1,16 @@
+package tujuhtujuh.Tugas;
+
+// Created by 21343043_Fadhil Nugraha Wikarya
+
+public class AngkaDo_While {
+    public static void main(String[] args) {
+        int i = 100;
+
+        do
+        {
+            System.out.println(i);
+            i--;
+        }
+        while (i >= 1);
+    }
+}
---
+++ src/tujuhtujuh/Tugas/NamaFor.java
@@ -0,0 +1,11 @@
+package tujuhtujuh.Tugas;
+
+// Created by 21343043_Fadhil Nugraha Wikarya
+
+public class NamaFor {
+    public static void main(String[] args) {
+        int bilangan;
+        for (bilangan = 0 ; bilangan <= 100 ; bilangan += 1)
+        System.out.println("Fadhil Nugraha Wikarya");
+       } 
+}
